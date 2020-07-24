5da67bc455f814d7707d5d4ff79f8eff1f6415d0
Shen Kuo <shen.kuo@iisigroup.com>
2020-07-24 10:24:32 +0800

    fix result encode error

--- parser.py
+++ parser.py
@@ -16,16 +16,18 @@ for f in os.listdir(zip_path):
 
 for f in os.listdir(source_path):
     txn = f[:5]
-    print(txn)
+    print('processing txn:',txn)
     #os.rename(os.path.join(source_path,f,'images'),os.path.join(source_path,f,'images_txn'+txn))
     #os.rename(os.path.join(source_path, f, 'style.css'), os.path.join(source_path, f, 'style_txn' + txn + '.css'))
-    shutil.copytree(os.path.join(source_path,f,'images'), os.path.join(result_path,'images_txn'+txn))
+    if os.path.isdir(os.path.join(source_path,f,'images')):
+        shutil.copytree(os.path.join(source_path,f,'images'), os.path.join(result_path,'images_txn'+txn))
+    else:
+        print('No images folder found')
     shutil.copy(os.path.join(source_path, f, 'style.css'), os.path.join(result_path, 'style_txn' + txn + '.css'))
 
     fin = open(os.path.join(source_path,f,'index.html'), 'rt',encoding="utf-8")
-    fout = open(os.path.join(result_path,'TXN'+txn+'.html'),'wt')
+    fout = open(os.path.join(result_path,'txn'+txn+'.html'),'wt',encoding="utf-8")
     for line in fin:
-        #print(line)
         fout.write(line.replace('src="images/', 'src="images_txn' + txn + '/').replace('style.css', 'style_txn'+txn+'.css'))
     fin.close()
     fout.close()
